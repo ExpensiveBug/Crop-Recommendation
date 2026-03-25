6d5efea6b010462c7bb932069b45eb440a00b8fd
ExpensiveBug <nidhireddu40@gmail.com>
2026-03-25 08:59:48 +0530

    user accounts

--- Crop_Recomendation/crop_site/recommend/urls.py
+++ Crop_Recomendation/crop_site/recommend/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home-view'),
+    path('signup/', views.signup, name = 'signup-view'),
+    path('predict/',views.prediction, name = 'predict-view'),
+    path('logout/',views.logout_page, name = 'logout-view'),
+    path('login/',views.login_page, name = 'login-view'),
 ]
