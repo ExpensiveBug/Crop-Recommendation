3e1e36aff725d0fef9228dd6db05abba59c3daa6
ExpensiveBug <nidhireddu40@gmail.com>
2026-03-25 09:03:36 +0530

    userprofile

--- Crop_Recomendation/crop_site/recommend/models.py
+++ Crop_Recomendation/crop_site/recommend/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 # Create your models here.
 
-class userprofile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.BigIntegerField()
 
