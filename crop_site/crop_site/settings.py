c533c10a697e3da58d4d5c3f53cf911b0c3f8f2d
ExpensiveBug <nidhireddu40@gmail.com>
2026-04-05 11:28:54 +0530

    db

--- crop_site/crop_site/settings.py
+++ crop_site/crop_site/settings.py
@@ -85,7 +85,9 @@ import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True  
     )
 }
 # DATABASES = {
