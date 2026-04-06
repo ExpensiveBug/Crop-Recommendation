19b44f0ee23b9869ec2eb1dd6f7b3ef7a1e8fb83
ExpensiveBug <nidhireddu40@gmail.com>
2026-04-06 19:58:56 +0530

    admin

--- crop_site/build.sh
+++ crop_site/build.sh
@@ -13,4 +13,20 @@ python3 manage.py collectstatic --noinput
 echo "===> Running Migrations..."
 python3 manage.py migrate --noinput
 
+python3 manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.getenv('ADMIN_USERNAME')
+password = os.getenv('ADMIN_PASSWORD')
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, 'admin@example.com', password)
+        print(f'Superuser {username} created successfully!')
+    else:
+        print(f'Superuser {username} already exists.')
+else:
+    print('Admin credentials not found in environment variables.')
+"
+
 echo "===> Build Completed!"
